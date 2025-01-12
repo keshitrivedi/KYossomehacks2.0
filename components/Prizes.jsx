17cfe5aa34d08ead029c4c94270bfc6adc8b425c
Yash <yb3028@srmist.edu.in>
2025-01-12 22:19:20 +0530

    Added a responsive design and fixed the images loading issue on Prizes page

--- components/Prizes.jsx
+++ components/Prizes.jsx
@@ -6,8 +6,7 @@ const Prizes = () => {
             <div>
             <div className='flex justify-center relative text-[60px] sm:text-[120px] md:text-[180px] lg:text-[280px] tracking-tight font-extrabold text-[#6B5E77]'>
                 PRIZES
-                <div className='flex justify-center absolute text-[30px] sm:text-[60px] md:text-[100px] lg:text-[140px] bottom-0 font-extrabold text-[#FCF961]'>
-                    PRIZES
+                <div className='flex justify-center absolute text-[30px] sm:text-[60px] md:text-[100px] lg:text-[140px] bottom-0 font-extrabold text-[#FCF961]'>PRIZES
                 </div>
             </div>
 
